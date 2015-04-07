3cfdbab12c6c129fcde50c3397cf59c23a9c88a5
jeansteve <ekenjean@gmail.com>
2015-04-07 08:41:43 +0200

    Modification de la récupération des paramètres  d'une page et je crée le chemin absolu en fonction des paramettres

--- www/wordpress/index.php
+++ www/wordpress/index.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: eken
- * Date: 19/03/2015
- * Time: 16:09
- */
+require 'vendor/autoload.php';
+
+ob_start();
+
+
+
+if(isset($_GET['page']))
+{
+    $page = $_GET['page'];
+    /**
+     * Si on passe des parametre a la page, je les stocke dans $com
+     */
+    if(strstr($page, '?'))
+    {
+        $page = explode("?", $page);
+        //echo $page[0];
+        $com = explode("=",$page[1]);
+        //echo $com[1];
+        $cheminPage = "./controleur/".$page[0].".php";
+
+    }
+    else
+    {
+        $cheminPage = "./controleur/".$page.".php";
+
+    }
+    include $cheminPage;
+}
+else
+{
+    include "vue/index.html";
+}
+$contenue = ob_get_clean();
+
+include "global/haut.php";
+
+echo $contenue;
+
+include "global/bas.php";
